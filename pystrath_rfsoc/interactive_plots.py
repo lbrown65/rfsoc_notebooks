fc04774a42c416492da9978c7879952d8d18d6e3
lewis brown <lewis.brown.101@strath.ac.uk>
2021-03-08 14:51:41 +0000

    Major updates to interactive_plots.py
    Fixed issues with Nyquist zone and sending data at initialization

--- pystrath_rfsoc/interactive_plots.py
+++ pystrath_rfsoc/interactive_plots.py
@@ -14,7 +14,7 @@ class ComplexFrequencyPlot():
                           'centre-freq' : 1024,
                           'height' : None,
                           'width' : None,
-                          #'c_data' : 0,
+                          'data' : 0,
                           'title' : 'Complex Frequency Plot',
                           'x-axis-title' : 'Frequency (Hz)',
                           'y-axis-title' : 'Amplitude'}
@@ -28,10 +28,9 @@ class ComplexFrequencyPlot():
         data = go.Scatter(
             x=np.arange(-self._config['sampling-freq']/2,
                         self._config['sampling-freq']/2,
-                        self._config['sampling-freq']/self._config['number-samples'])
-            + self._config['centre-freq'],
-            y=np.zeros(self._config['number-samples']))
-            #y= freq)
+                        self._config['sampling-freq']/self._config['number-samples']),
+            #+ self._config['centre-freq']*1e6,
+            y= self._config['data'])
 
         self._plot = go.FigureWidget(
             data=data,
@@ -96,13 +95,11 @@ class ComplexTimePlot():
 
         data_re = go.Scatter(
             x=np.arange(0, self._config['number-samples']/self._config['sampling-freq'], 1/self._config['sampling-freq']),
-            #y=np.zeros(2048),
             y=np.real(self._config['c_data']),
             name='Real')
 
         data_im = go.Scatter(
             x=np.arange(0, self._config['number-samples']/self._config['sampling-freq'], 1/self._config['sampling-freq']),
-            #y=np.zeros(2048),
             y=np.imag(self._config['c_data']),
             name='Imag')
 
@@ -146,7 +143,7 @@ class ComplexTimePlot():
     def get_plot(self):
         return self._plot
     
-class ToneGenerator():
+class DAC_ToneGenerator():
 
     def __init__(self,
                  channel,
@@ -194,16 +191,20 @@ class ADC_ToneGenerator():
     @adc_centre_frequency.setter
     def adc_centre_frequency(self, adc_centre_frequency):
         block = self._channel.adc_block
-        if (adc_centre_frequency == 0):
-            adc_centre_frequency = 1
-        elif (adc_centre_frequency < -block.BlockStatus['SamplingFreq']*1e3) \
+        #if (adc_centre_frequency == 0):
+            #adc_centre_frequency = 1
+        if (adc_centre_frequency < -block.BlockStatus['SamplingFreq']*1e3) \
             or (adc_centre_frequency > block.BlockStatus['SamplingFreq']*1e3):
             raise ValueError ('ADC Centre frequency out of range')
-        zone = block.NyquistZone
-        even = True if ((zone % 2) == 0) else False
-        req_zone = int(np.ceil(abs(adc_centre_frequency)/((block.BlockStatus['SamplingFreq']*1e3)/2)))
-        if req_zone != zone:
-            block.NyquistZone = req_zone
+        if (adc_centre_frequency == 0): 
+            zone = 1
+            even = True if ((zone % 2) == 0) else False
+        else:
+            zone = block.NyquistZone
+            req_zone = int(np.ceil(abs(adc_centre_frequency)/((block.BlockStatus['SamplingFreq']*1e3)/2)))
+            if req_zone != zone:
+                block.NyquistZone = req_zone
+        even = True if ((zone % 2) == 0) else False       
         if even:
             block.MixerSettings['Freq'] = adc_centre_frequency
         else:
@@ -252,7 +253,6 @@ class CoarseMixerApplication():
                  sample_frequency=4096e6,
                  number_samples=2048,
                  centre_frequency=1024,
-                 #desired_frequency=0,
                  window='blackman',
                  height=None,
                  width=None):
@@ -282,22 +282,21 @@ class CoarseMixerApplication():
             self.frequency_plot.update_data(freq)
             
         def set_desired_freq(widget):
-            #centre_frequency = widget['new']*1e6
             centre_frequency = widget['new']
-            self.tone_generator.centre_frequency = centre_frequency
+            self.dac_tone_generator.centre_frequency = centre_frequency
             c_data = rx_channel.transfer(packetsize = number_samples)
             self.time_plot.update_data(c_data)
             freq = self.frequency_processor.convert_to_freq(c_data)
             self.frequency_plot.update_data(freq)
 
-        self.tone_generator = ToneGenerator(tx_channel, centre_frequency)
+        self.dac_tone_generator = DAC_ToneGenerator(tx_channel, centre_frequency)
 
         self.frequency_processor = FrequencyProcessor(configuration={
             'sampling-freq'  : sample_frequency/2,
             'window'         : window})
         
         c_data = rx_channel.transfer(packetsize=number_samples)
-        #freq = self.frequency_processor.convert_to_freq(c_data)
+        freq = self.frequency_processor.convert_to_freq(c_data)
 
         self.time_plot = ComplexTimePlot({
             'sampling-freq'  : sample_frequency,
@@ -315,17 +314,15 @@ class CoarseMixerApplication():
             'centre-freq'    : centre_frequency,
             'height'         : height,
             'width'          : width,
-            #'data'           : c_data,
+            'data'           : freq,
             'title'          : 'Receiver: Complex Frequency Plot',
             'x-axis-title'   : 'Frequency (Hz)',
             'y-axis-title'   : 'Power Spectral Density (dB)'})
 
         self.desired_freq_slider = ipw.FloatSlider(
-            #value=(centre_frequency)*1e-6,
             value=(centre_frequency),
             min=1,
             max=(sample_frequency/2)*1e-6,
-            #max=(sample_frequency/2),
             step=1,
             description='Transmitter Frequency:',
             disabled=False,
@@ -335,7 +332,9 @@ class CoarseMixerApplication():
             style = {'description_width': 'initial'})
         
         self.desired_coarse_mix_freq_dropdown = ipw.Dropdown(
-            options=[('fs/2', xrfdc.COARSE_MIX_SAMPLE_FREQ_BY_TWO), ('fs/4', xrfdc.COARSE_MIX_SAMPLE_FREQ_BY_FOUR), ('-fs/4', xrfdc.COARSE_MIX_MIN_SAMPLE_FREQ_BY_FOUR)],
+            options=[('fs/2', xrfdc.COARSE_MIX_SAMPLE_FREQ_BY_TWO), 
+                     ('fs/4', xrfdc.COARSE_MIX_SAMPLE_FREQ_BY_FOUR), 
+                     ('-fs/4', xrfdc.COARSE_MIX_MIN_SAMPLE_FREQ_BY_FOUR)],
             value=xrfdc.COARSE_MIX_SAMPLE_FREQ_BY_FOUR,
             description='Mix Freq:',
             disabled=False,
@@ -374,7 +373,7 @@ class FineMixerApplication():
         rx_channel.adc_block.MixerSettings.update({
                 'MixerType': xrfdc.MIXER_TYPE_FINE,
                 'FineMixerScale': xrfdc.MIXER_SCALE_1P0,
-                'Freq': 1024
+                'Freq': -1024
             })
         
         tx_channel.control.gain = 0.5
@@ -382,7 +381,7 @@ class FineMixerApplication():
             
         def set_desired_freq(widget):
             centre_frequency = widget['new']
-            self.tone_generator.centre_frequency = centre_frequency
+            self.dac_tone_generator.centre_frequency = centre_frequency
             c_data = rx_channel.transfer(packetsize = number_samples)
             self.time_plot.update_data(c_data)
             freq = self.frequency_processor.convert_to_freq(c_data)
@@ -396,7 +395,7 @@ class FineMixerApplication():
             freq = self.frequency_processor.convert_to_freq(c_data)
             self.frequency_plot.update_data(freq)
 
-        self.tone_generator = ToneGenerator(tx_channel, centre_frequency)
+        self.dac_tone_generator = DAC_ToneGenerator(tx_channel, centre_frequency)
         
         self.adc_tone_generator = ADC_ToneGenerator(rx_channel, centre_frequency)
 
@@ -405,7 +404,7 @@ class FineMixerApplication():
             'window'         : window})
         
         c_data = rx_channel.transfer(packetsize=number_samples)
-        #freq = self.frequency_processor.convert_to_freq(c_data)
+        freq = self.frequency_processor.convert_to_freq(c_data)
 
         self.time_plot = ComplexTimePlot({
             'sampling-freq'  : sample_frequency,
@@ -423,7 +422,7 @@ class FineMixerApplication():
             'centre-freq'    : centre_frequency,
             'height'         : height,
             'width'          : width,
-            #'data'           : c_data,
+            'data'           : freq,
             'title'          : 'Receiver: Complex Frequency Plot',
             'x-axis-title'   : 'Frequency (Hz)',
             'y-axis-title'   : 'Power Spectral Density (dB)'})
